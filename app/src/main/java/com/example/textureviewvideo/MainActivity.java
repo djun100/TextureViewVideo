3de3e141b6fe6a715b31016fc514aa6ce3921a75
wangxuechao <djun100@qq.com>
2020-03-16 15:20:45 +0800

    精简

--- app/src/main/java/com/example/textureviewvideo/MainActivity.java
+++ app/src/main/java/com/example/textureviewvideo/MainActivity.java
@@ -7,40 +7,19 @@ import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
-import android.view.View;
 import android.view.TextureView.SurfaceTextureListener;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 
 public class MainActivity extends Activity{
-	private final String Tag = MainActivity.class.getSimpleName();
 
 	private MediaPlayer mMediaPlayer;
 	private Surface surface;
 	
 	private ImageView videoImage;
-	private SeekBar seekBar;
 
-	private Handler handler=new Handler();
-
-	private final Runnable mTicker = new Runnable(){
-		public void run() {
-			long now = SystemClock.uptimeMillis();
-			long next = now + (1000 - now % 1000);
-
-			handler.postAtTime(mTicker,next);//延迟一秒再次执行runnable,就跟计时器一样效果
-
-			if(mMediaPlayer!=null&&mMediaPlayer.isPlaying()){
-				seekBar.setProgress(mMediaPlayer.getCurrentPosition());//更新播放进度
-			}
-		}
-	};
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,9 +29,6 @@ public class MainActivity extends Activity{
 		textureView.setSurfaceTextureListener(surfaceTextureListener);//设置监听函数  重写4个方法
 
 		videoImage=(ImageView) findViewById(R.id.video_image);
-
-		seekBar= (SeekBar) findViewById(R.id.seekbar);
-		seekBar.setOnSeekBarChangeListener(onSeekBarChangeListener);//seekbar改变监听
 	}
 
 	private SurfaceTextureListener surfaceTextureListener=new SurfaceTextureListener() {
@@ -96,9 +72,6 @@ public class MainActivity extends Activity{
 					public void onPrepared(MediaPlayer mp){//预加载完成
 						videoImage.setVisibility(View.GONE);//隐藏图片
 						mMediaPlayer.start();//开始播放
-
-						seekBar.setMax(mMediaPlayer.getDuration());//设置总进度
-						handler.post(mTicker);//更新进度
 					}
 				  });
 				  mMediaPlayer.prepare();
@@ -108,29 +81,10 @@ public class MainActivity extends Activity{
 	    }
 	}
 
-	private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener=new SeekBar.OnSeekBarChangeListener() {
-		@Override
-		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {//进度改变
-		}
-
-		@Override
-		public void onStartTrackingTouch(SeekBar seekBar) {//开始拖动seekbar
-		}
-
-		@Override
-		public void onStopTrackingTouch(SeekBar seekBar) {//停止拖动seekbar
-			if(mMediaPlayer!=null&&mMediaPlayer.isPlaying()){//播放中
-				mMediaPlayer.seekTo(seekBar.getProgress());
-			}
-		}
-	};
-
 	private MediaPlayer.OnCompletionListener onCompletionListener=new MediaPlayer.OnCompletionListener() {
 		@Override
 		public void onCompletion(MediaPlayer mediaPlayer) {//播放完成
 			videoImage.setVisibility(View.VISIBLE);
-			seekBar.setProgress(0);
-			handler.removeCallbacks(mTicker);//删除执行的Runnable 终止计时器
 		}
 	};
 }
